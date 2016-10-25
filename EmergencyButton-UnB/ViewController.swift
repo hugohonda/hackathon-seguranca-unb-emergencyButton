547a2d6b4c7170263427061f272b7af6bfd9b84d
Hugo Honda Ferreira <hugohonda@HugoHonda.local>
2016-10-25 10:51:12 -0200

    first commit

--- EmergencyButton-UnB/ViewController.swift
+++ EmergencyButton-UnB/ViewController.swift
@@ -1,25 +1,56 @@
-//
-//  ViewController.swift
-//  EmergencyButton-UnB
-//
-//  Created by Hugo Honda Ferreira on 24/10/16.
-//  Copyright © 2016 Hugo Honda Ferreira. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var headphonePluggedInStateImageView: UIImageView!
+    let deviceRequiredImage = "device_required"
+    let headphonePluggedInMessage = "headphone_plugged_in"
+    let headphonePulledOutMessage = "headphone_pulled_out"
+    
+    @IBOutlet weak var isConnectedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let currentRoute = AVAudioSession.sharedInstance().currentRoute
+        
+        if currentRoute.outputs.count != 0 {
+            for description in currentRoute.outputs {
+                if description.portType == AVAudioSessionPortHeadphones {
+                    isConnectedLabel.text = "plugado!"
+                    print("headphone plugged in")
+                } else {
+                    isConnectedLabel.text = "desplugado!"
+                    print("headphone pulled out")
+                }
+            }
+        } else {
+            isConnectedLabel.text = "requer conexão"
+            print("requires connection to device")
+        }
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(ViewController.audioRouteChangeListener(_:)),
+            name: NSNotification.Name.AVAudioSessionRouteChange,
+            object: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    dynamic fileprivate func audioRouteChangeListener(_ notification:Notification) {
+        let audioRouteChangeReason = (notification as NSNotification).userInfo![AVAudioSessionRouteChangeReasonKey] as! UInt
+        
+        switch audioRouteChangeReason {
+        case AVAudioSessionRouteChangeReason.newDeviceAvailable.rawValue:
+            isConnectedLabel.text = "plugado!"
+            print("headphone plugged in")
+        case AVAudioSessionRouteChangeReason.oldDeviceUnavailable.rawValue:
+            isConnectedLabel.text = "desplugado!"
+            print("headphone pulled out")
+        default:
+            break
+        }
     }
-
-
+    
+    
 }
 
